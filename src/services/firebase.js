a20b7f90f5299a58c4151180ce23217efd9ed7c1
devansh5 <devkumar5436@gmail.com>
2020-11-27 05:20:46 +0530

    added firestore as db

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -6,7 +6,7 @@ import dotenv from 'dotenv'
 import { Redirect } from "react-router-dom";
 dotenv.config()
 
-firebase.initializeApp({
+export default firebase.initializeApp({
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
